c8f191bc70285c564639edf3ab70d6f5bee936e5
T_at_JetsonNano_Str <takap.colonel@gmail.com>
2020-06-22 01:13:40 +0900

    温度読み出しのフォーマットを微修正

--- systemp_exe.py
+++ systemp_exe.py
@@ -15,24 +15,24 @@ sns_temp = []
 if len(args) == 1:
   sns_name, sns_temp = readsystemp(ARG_MODE_ALL_STR)
   for sns, temp in zip(sns_name, sns_temp):
-    print(sns + "\t: " + str(temp) + "degC")
+    print(sns + "\t: " + str(temp) + " degC")
   sys.exit(1)
 elif args[1] == ARG_MODE_ALL_STR:
   sns_name, sns_temp = readsystemp(ARG_MODE_ALL_STR)
   for sns, temp in zip(sns_name, sns_temp):
-    print(sns + "\t: " + str(temp) + "degC")
+    print(sns + "\t: " + str(temp) + " degC")
   sys.exit(0)
 elif args[1] == ARG_MODE_MIN_STR:
   sns_name, sns_temp = readsystemp(ARG_MODE_MIN_STR)
-  print(sns_name + "\t: " + str(sns_temp) + "degC")
+  print(sns_name + "\t: " + str(sns_temp) + " degC")
   sys.exit(0)
 elif args[1] == ARG_MODE_MAX_STR:
   sns_name, sns_temp = readsystemp(ARG_MODE_MAX_STR)
-  print(sns_name + "\t: " + str(sns_temp) + "degC")
+  print(sns_name + "\t: " + str(sns_temp) + " degC")
   sys.exit(0)
 elif args[1] == ARG_MODE_AVE_STR:
   sns_name, sns_temp = readsystemp(ARG_MODE_AVE_STR)
-  print("AVerage \t: " + str(sns_temp) + "degC")
+  print("AVerage \t: " + str(sns_temp) + " degC")
   sys.exit(0)
 else:
   print("Invalid Arg")
